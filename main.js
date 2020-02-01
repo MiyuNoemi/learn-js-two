40ca5e1eabb30c61466ade82eca5c1a1654c7218
WeronikaBrzóska <weronikabrzoska97@gmail.com>
2020-02-01 14:13:24 +0100

    suitable colour for even and odd

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 document.body.addEventListener('click', (event) => {
-            const x = event.clientX;
-            const y = event.clientY;
-            const clientX = `${x}`;
-            const clientY = `${y}`;
-        }
+    const x = event.clientX;
+    const y = event.clientY;
+    const clientX = `${x}`;
+    const clientY = `${y}`;
+
+    if (clientX % 2 == 0 && clientY % 2 == 0) {
+        document.body.style.backgroundColor = 'red';
+    } else if (clientX % 2 != 0 && clientY % 2 != 0) {
+        document.body.style.backgroundColor = 'blue';
+    } else if (clientX % 2 == 0 && clientY % 2 != 0) {
+        document.body.style.backgroundColor = 'green';
+    } else if (clientX % 2 != 0 && clientY % 2 == 0) {
+        document.body.style.backgroundColor = 'green';
+    }
+
+    console.log(clientX, clientY)
+})
